e1ccfba768327d2d9fcba4de3fb43e26964a5517
Ignacio <ign.souto@gmail.com>
2023-09-11 19:20:33 +0200

    New webapp incomming :rocket:

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import cx from "classnames";
+import { sfPro, inter } from "./fonts";
+import Nav from "@/components/layout/nav";
+import Footer from "@/components/layout/footer";
+import { Suspense } from "react";
+
+export const metadata = {
+  title: "Dipassio - Building Devs for your Next Project",
+  description:
+    "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
+  twitter: {
+    card: "summary_large_image",
+    title: "Dipassio - Building Devs for your Next Project",
+    description:
+      "Dipassio is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
+    creator: "@steventey",
+  },
+  metadataBase: new URL("https://dipass.io/en"),
+  themeColor: "#FFF",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cx(sfPro.variable, inter.variable)}>
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+        <Suspense fallback="...">
+          {/* @ts-expect-error Server Component */}
+          <Nav />
+        </Suspense>
+        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+          {children}
+        </main>
+        <Footer />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
